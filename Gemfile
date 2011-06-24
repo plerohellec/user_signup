795208c72335e7bca53be0e8387eaa833e8f252a
Philippe Le Rohellec <philippe@lerohellec.com>
2011-06-23 17:23:29 -0700

    initial checkin

---
+++ Gemfile
@@ -0,0 +1,13 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.7'
+
+gem 'mysql2', '0.2.6'
+gem 'will_paginate', '3.0.pre2'
+
+group :development, :test do
+    gem 'rspec-rails'
+    gem 'factory_girl_rails'
+    gem 'webrat'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UserSignup::Application.config.secret_token = '878ea70496750049bb9ee253d3f907ba938fa362a5c50cb9ade77b72d25cfc1dbfb6fef19193f655ecbd3ecfe535bcbaf5a5ec03e2adca747eeb90fca77cf78b'
